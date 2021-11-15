33144ed7188be9957d6e365275fcf6b05da3c615
hritik chouhan <you@example.com>
2021-11-15 12:44:48 +0530

    first

---
+++ app/src/main/java/com/example/q1ca2sol/qu1ca1sol.kt
@@ -0,0 +1,12 @@
+package com.example.q1ca2sol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class qu1ca1sol : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qu1ca1sol)
+
+    }
+}
